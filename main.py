b05cdca640505393c3145e692b5cea01be100be4
tarrows <rem_tate@yahoo.co.jp>
2021-05-06 23:15:23 +0900

    minimum annotate api

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import spacy
+
 from fastapi import FastAPI
 
 app = FastAPI()
+nlp = spacy.load('ja_ginza')
 
 
 @app.get('/')
 async def index():
     return {'hello': 'world'}
+
+
+@app.get('/api/annotate')
+async def annotate(text: str):
+    doc = nlp(text)
+    return doc.to_json()
